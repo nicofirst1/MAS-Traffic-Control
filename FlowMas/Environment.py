1de75b9e9142845cf96e2a4f6d33d2835396b32e
Dizzi <brandimax2+gitkraken@gmail.com>
2019-09-11 17:31:04 +0200

    adding empty files

---
+++ FlowMas/Environment.py
@@ -0,0 +1,6 @@
+# import the base environment class
+from flow.envs import Env
+
+# define the environment class, and inherit properties from the base environment class
+class myEnv(Env):
+    pass
---
+++ FlowMas/Scenario.py
@@ -0,0 +1,6 @@
+# import Flow's base scenario class
+from flow.scenarios import Scenario
+
+# define the scenario class, and inherit properties from the base scenario class
+class myScenario(Scenario):
+    pass
