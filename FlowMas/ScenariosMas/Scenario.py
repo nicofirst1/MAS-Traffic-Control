f9ac9907f3b89c1222b8e338e3f0c3b7629236f9
Dizzi <brandimax2+gitkraken@gmail.com>
2019-09-11 17:40:57 +0200

    Split scenarios

--- FlowMas/ScenariosMas/Scenario.py
+++ FlowMas/ScenariosMas/Scenario.py
@@ -1,3 +1,6 @@
 # import Flow's base scenario class
-from flow.scenarios.grid import SimpleGridScenario
+from flow.scenarios import Scenario
 
+# define the scenario class, and inherit properties from the base scenario class
+class myScenario(Scenario):
+    pass
