b8e2a29f80d4824d636a362f9af6d0e75941c9b6
Dizzi <brandimax2+gitkraken@gmail.com>
2019-09-18 11:19:17 +0200

    add customRL in init

--- flow/envs/multiagent/__init__.py
+++ flow/envs/multiagent/__init__.py
@@ -7,6 +7,7 @@ from flow.envs.multiagent.ring.wave_attenuation import \
 from flow.envs.multiagent.ring.accel import MultiAgentAccelEnv
 from flow.envs.multiagent.traffic_light_grid import MultiTrafficLightGridPOEnv
 from flow.envs.multiagent.highway import MultiAgentHighwayPOEnv
+from flow.envs.multiagent.customRL import CustoMultiRL
 
 __all__ = ['MultiEnv', 'MultiAgentAccelEnv', 'MultiWaveAttenuationPOEnv',
-           'MultiTrafficLightGridPOEnv', 'MultiAgentHighwayPOEnv']
+           'MultiTrafficLightGridPOEnv', 'MultiAgentHighwayPOEnv','CustoMultiRL']
