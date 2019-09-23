cfac3c51bdc8a14d1555cb2ced0fbe9e38ebf659
Dizzi <brandimax2+gitkraken@gmail.com>
2019-09-23 18:28:32 +0200

    moved script dir

--- scripts/kill_dmas.sh
+++ scripts/kill_dmas.sh
@@ -3,6 +3,7 @@ num_ps=$(ps -ax | grep dmas | grep python| wc -l)
 if [ $num_ps -gt 0 ];then
     echo "There are currently $num_ps processes running under the dmas folder\n Killing..."
     pkill -f dmas
+    sleep 1
     num_ps=$(ps -ax | grep dmas | grep python| wc -l)
     echo "Number of precess left is $num_ps "
 else
