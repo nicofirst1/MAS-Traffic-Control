c5565ba63fa044c9061b6137e2c74290993ff147
dizzi <nicofirst1@gmail.com>
2019-09-27 09:11:26 +0200

    add mac/ubutnu installation

--- scripts/python_setup.sh
+++ scripts/python_setup.sh
@@ -1,4 +1,4 @@
-
+#!bin/bash
 # update conda
 conda update conda -y
 
@@ -7,24 +7,38 @@ echo "Creating enviroment..."
 
 conda create --name dmas python=3.6
 
+source activate dmas
+
 # install requirements
 echo "Installing requirements..."
-pip install -y -r requirements.txt
+pip install -r requirements.txt
 
 
 # Install flow develop
 echo "Installing ray..."
 
 git clone https://github.com/flow-project/ray.git
-python ray/python/setup.py develop
+cd ray/python/
+python setup.py develop
+cd ../..
 
-# remove ray 
-rm -rf ray
 
 # call ubuntu install script for sumo 
 echo "Updating..."
 
+
+os="$(uname)"
+
+
+
+if [ $os = "Darwin" ]; then
+sh scripts/setup_sumo_macosx.sh
+
+else
+
 sh scripts/setup_sumo_ubuntu1804.sh
+fi
+
 
 # install sumo tools
 pip install https://akreidieh.s3.amazonaws.com/sumo/flow-0.4.0/sumotools-0.4.0-py3-none-any.whl
