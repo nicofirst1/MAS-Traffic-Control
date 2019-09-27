3ac87ba3ac8f83df460f8e79f1cdf7fcc5b5b699
dizzi <nicofirst1@gmail.com>
2019-09-27 18:36:54 +0200

    moved script before setup

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 from distutils.command.sdist import sdist as sdist_orig
 from distutils.errors import DistutilsExecError
+import subprocess
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 
-class sdist(sdist_orig):
-
-    def run(self):
-        try:
-            self.spawn(['sh', './scripts/initial_config.sh'])
-        except DistutilsExecError:
-            self.warn('Could not run config script, please run it using "sh ./scripts/initial_config.sh"')
-        super().run()
 
 
+cmds=['sh', './scripts/initial_config.sh']
+MyOut = subprocess.Popen(cmds,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+stdout,stderr = MyOut.communicate()
+print(stdout)
+print(stderr)
 
 setup(
     name='dmas',
@@ -28,9 +28,6 @@ setup(
     author='Dizzibus',
     install_requires=requirements,
     description='Project for Design of Multi-Agent Systems KIM.DMAS04.2019-2020.1A, University of GroningenOSM ',
-    cmdclass={
-        'sdist': sdist
-    },
-
+   
 
 )
