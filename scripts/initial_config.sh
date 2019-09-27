aa0ab15e23f5f248bddb80b12ec3f35d4b62a63a
dizzi <nicofirst1@gmail.com>
2019-09-27 15:45:53 +0200

    fixed config scripts

--- scripts/initial_config.sh
+++ scripts/initial_config.sh
@@ -6,11 +6,11 @@ installation_instruction="MarkDown/Installation.md"
 
 if [ $os = "Darwin" ]; then
 # mac system
-    sh ./scripts/initial_configs/initial_config_macosx.sh
+    sh ./scripts/initial_configs/macOSX.sh
 
 elif [ $os = "Linux" ]; then
     # installing for ubuntu
-    sh ./scripts/initial_configs/initial_config_ubuntu.sh
+    sh ./scripts/initial_configs/ubuntu18.sh
 
 else
   echo "This operating system is not supported yet! Please use the installation instruction in $installation_instruction"
--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
+from distutils.command.sdist import sdist as sdist_orig
+from distutils.errors import DistutilsExecError
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
+
+class sdist(sdist_orig):
+
+    def run(self):
+        try:
+            self.spawn(['sh', './scripts/initial_config.sh'])
+        except DistutilsExecError:
+            self.warn('Could not run config script, please run it using "sh ./scripts/initial_config.sh"')
+        super().run()
+
 
 
 setup(
@@ -14,4 +27,10 @@ setup(
     include_package_data=True,
     author='Dizzibus',
     install_requires=requirements,
-    description='Project for Design of Multi-Agent Systems KIM.DMAS04.2019-2020.1A, University of GroningenOSM ',)
+    description='Project for Design of Multi-Agent Systems KIM.DMAS04.2019-2020.1A, University of GroningenOSM ',
+    cmdclass={
+        'sdist': sdist
+    },
+
+
+)
