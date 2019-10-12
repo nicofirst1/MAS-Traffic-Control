392306678adc1bc6b59805630d33fe2ec7c1f417
dizzi <nicofirst1@gmail.com>
2019-10-12 17:07:48 +0200

    fixed reqs
    clean setup

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
-import subprocess
 
-import setuptools.command.build_ext as _build_ext
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
-
-class build_ext(_build_ext.build_ext):
-    def run(self):
-        command = ['./scripts/initial_config.sh']
-        subprocess.check_call(command)
 
 
 setup(
@@ -20,7 +12,7 @@ setup(
     license='MIT',
     url='https://gitlab.com/nicofirst1/mas_traffic',
     include_package_data=True,
-    author='Dizzibus',
+    author='Nicolo Brandizzi',
     install_requires=requirements,
     description='Project for Design of Multi-Agent Systems KIM.DMAS04.2019-2020.1A, University of GroningenOSM ',
 
