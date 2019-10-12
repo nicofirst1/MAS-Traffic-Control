f7c2b3b2b2614564f45df51409c9e5c4a6437566
dizzi <nicofirst1>
2019-10-12 12:30:17 +0200

    add kill sumo

--- scripts/kill_dmas.sh
+++ scripts/kill_dmas.sh
@@ -1,8 +1,10 @@
 num_ps=$(ps -ax | grep dmas | grep python| wc -l)
+num_ps=$(($num_ps+$(ps -ax | grep sumo| grep -v grep| wc -l)))
 
 if [ $num_ps -gt 0 ];then
-    echo "There are currently $num_ps processes running under the dmas folder\n Killing..."
+    echo "There are currently $num_ps processes running under the dmas folder. Killing..."
     pkill -f dmas
+    pkill -f sumo
     sleep 1
     num_ps=$(ps -ax | grep dmas | grep python| wc -l)
     echo "Number of precess left is $num_ps "
